bce54537b0856eb05d94ae22e632ac1d63f566ef
suresh <sureshpradhana19@gmail.com>
2024-01-28 09:28:35 +0530

    added grid for images in creativecoding

--- src/components/svelte/CreativeCoding.svelte
+++ src/components/svelte/CreativeCoding.svelte
@@ -1,37 +1,70 @@
 <script>
-	let projects=[
+	let projects = [
 		{
 			name: "wall Drawing 11 (1969)",
 			link: "creative_coding/wallDrawing11",
-			description: "A wall divided horizontally and vertically into four equal parts. Within each part, three of the four kinds of lines are superimposed."
-		},	
+			description:
+				"A wall divided horizontally and vertically into four equal parts. Within each part, three of the four kinds of lines are superimposed.",
+			imageurl: "./public/images/walldrawing11.png",
+		},
 
 		{
 			name: "sphere",
 			link: "creative_coding/sphere",
-			description: "A sphere with a Threejs"
-		}
-	
-
-	]
+			description: "A sphere with a Threejs",
+			imageurl: "./public/images/sphere.png",
+		},
+	];
 </script>
 
 <div>
 	<h1>creative coding solutions!</h1>
-	<p>
+
+	<!-- <p>
 		Here are my creative coding solutions. I am using canvas-sketch and Threejs to create these solutions.
 	</p>
 	<ul>
+		TODO instead of using texts use images as grid and try they should be dynamically generated
 		{#each projects as project (project) }
 			<li>
 			<a href={project.link}>{project.name}</a>: {project.description}
 		</li>
 			
 		{/each}
-	</ul>
+	</ul> -->
+
+	<div class="grid-wrapper">
+		{#each projects as project (project)}
+			<div class="grid-item">
+				<a href={project.link}>
+					<img src={project.imageurl} alt={project.name} />
+				</a>
+			</div>
+		{/each}
+	</div>
 </div>
 
 <style>
+	.grid-wrapper {
+		display: grid;
+		grid-template-columns: repeat(4, 1fr);
+		grid-auto-flow: column;
+		grid-gap: 1rem;
+	}
+	.grid-item {
+		width: 100%;
+		height: 200px;
+		overflow: hidden;
+	}
+	img {
+		width: 100%;
+		height: 100%;
+		transition: transform 0.3s ease;
+	}
+
+	img:hover {
+		transform: scale(1.1);
+	}
 	h1 {
 		color: var(--red);
 		font-size: var(--h1);
@@ -54,25 +87,13 @@
 		background-color: var(--blue);
 		color: var(--white);
 	}
-	a::after {
-		content: "]";
-		display: inline-block;
-		width: 0;
-		height: 2px;
-		background-color: var(--blue);
-		transition: width 0.3s ease-in-out;
-		margin: 0 0.5rem 0 0.5rem;
-	}
-	a::before {
-		content: "[";
-		display: inline-block;
-		width: 0;
-		height: 2px;
-		background-color: var(--blue);
-		transition: width 0.3s ease-in-out;
-		margin: 0 1rem 0 0rem;
-	}
+
 	li::marker {
 		color: white;
+	}
+	@media (max-width: 600px) {
+		.grid-wrapper {
+			grid-template-columns: repeat(2, auto);
+		}
 	}
 </style>
