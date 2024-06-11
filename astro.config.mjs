d0b9d949557bebfbe3aa10a03411736f670ddbe3
SureshPradhana <sureshpradhana19@gmail.com>
2024-06-11 20:07:36 +0530

    chore: reading time

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import { remarkReadingTime } from './remark-reading-time.mjs';
 import tailwind from "@astrojs/tailwind";
 
 import svelte from "@astrojs/svelte";
@@ -8,5 +9,8 @@ export default defineConfig({
   site: 'https://suresh-pradhana.vercel.app/',
   integrations: [tailwind(), svelte({
     include: ["**/svelte/*"]
-  })]
+  })],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
 });
