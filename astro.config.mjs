58abbe348dc7ccb3399e6070ed6ae99f7bc5c90b
SureshPradhana <sureshpradhana19@gmail.com>
2024-06-21 13:11:13 +0530

    chore: seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,14 @@ import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import auth from 'auth-astro';
 import vercel from '@astrojs/vercel/serverless';
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://suresh-pradhana.vercel.app/',
   integrations: [tailwind(), svelte({
     include: ["**/svelte/*"]
-  }), auth()],
+  }), auth(),robotsTxt()],
   markdown: {
     remarkPlugins: [remarkReadingTime]
   },
