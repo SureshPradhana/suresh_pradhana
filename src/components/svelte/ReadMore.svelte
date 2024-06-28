5f8b34bd93a78b25eeca6e19dc7683e173b92874
SureshPradhana <sureshpradhana19@gmail.com>
2024-06-28 18:20:12 +0530

    chore: more posts section

---
+++ src/components/svelte/ReadMore.svelte
@@ -0,0 +1,32 @@
+<script>
+    export let refPosts = [];
+</script>
+
+<div>
+    <section class="blog-entries">
+        <h2>Posts</h2>
+        {#each refPosts.slice(0, 3) as post}
+            <div class="post">
+                <div class="flex justify-between items-baseline">
+                    <div>
+                        <h3 class="text-[14px]">
+                            <a
+                                href={`/refcards/${post.slug}`}
+                                class="text-gray-300 border-none font-thin"
+                                >{post.data.title}</a
+                            >
+                        </h3>
+                    </div>
+                    <div>
+                        <time datetime={post.data.date.toISOString()}>
+                            {post.data.date.toDateString()}
+                        </time>
+                    </div>
+                </div>
+            </div>
+        {/each}
+        <div class="flex justify-end">
+            <a href="/refcards" class="">View All</a>
+        </div>
+    </section>
+</div>
